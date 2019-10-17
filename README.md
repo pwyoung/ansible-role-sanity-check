6a9781403fc9c406e16eb575dceb0fb9af3add4b
pwyoung <phil.w.young@gmail.com>
2019-10-17 18:58:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Goal
+This is a basic sanity test of Ansible
+
+
+# Dev/Test
+This role includes a subdirectory with a playbook that can be useful
+for doing development on this role.
+
+To run the test, just issue
+```
+make
+```
+
+See ./test/README.md for more details on development and testing this role.
+
+# License
+
+GPLv2
---
+++ python/test_script.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def main():
+    test_msg = "Default Test Message in Python Script"
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--test-message',
+                        action='store', type=str, dest="test_msg",
+                        help='Test Message',
+                        default=test_msg)
+    args = parser.parse_args()
+
+    print("Test message is: " + args.test_msg)
+
+if __name__ == "__main__":
+    main()
